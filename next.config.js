c8505897e435205a18f761f758b9b282eff3c52c
Trevor Adams <nytadams@familysearch.org>
2020-12-20 13:37:28 -0700

    adding var to optimize css

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
     domains: ['firebasestorage.googleapis.com'],
   },
   env: {
+    NODE_ENV: process.env.NODE_ENV,
     BASE_URL: process.env.BASE_URL,
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
